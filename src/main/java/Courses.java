b2708ac6514301b8152cec14acadde6638cc8e7f
Onglasbekov-Almas <154738069+Onglasbekov-Almas@users.noreply.github.com>
2024-02-09 23:36:25 +0600

    Add files via upload

---
+++ src/main/java/Courses.java
@@ -0,0 +1,19 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Courses {
+    private int id;
+    private int teacher_id;
+
+
+    @Override
+    public String toString() {
+        return id + " " + teacher_id;
+    }
+}
---
+++ src/main/java/Student.java
@@ -0,0 +1,22 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Student {
+    private int id;
+    private String surname;
+    private String name;
+    private int age;
+    private int email;
+
+
+    @Override
+    public String toString() {
+        return id + " " + name + " " + surname + " " + age + " " + email;
+    }
+}
